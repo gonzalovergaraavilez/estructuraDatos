564ec43aa71d9fd64f8f1ba4f7a19522ed173a3a
Gonzalo Vergara <gonzalo.va@me.com>
2015-01-28 00:39:15 -0600

    Addes HW 1_1 & 1_2

---
+++ tarea_1_1/tarea_1_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tarea_1_1
+//
+//  Created by Gonzalo Vergara on 1/27/15.
+//  Copyright (c) 2015 Gonzalo Vergara. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ tarea_1_2/tarea_1_2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tarea_1_2
+//
+//  Created by Gonzalo Vergara on 1/28/15.
+//  Copyright (c) 2015 Gonzalo Vergara. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
