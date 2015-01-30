d6b4c030e6bb465790c58886ad1ee30c6e9c402b
Gonzalo Vergara <gonzalo.va@me.com>
2015-01-29 22:49:36 -0600

    hw1

--- tarea_1_1/tarea_1_1/main.cpp
+++ tarea_1_1/tarea_1_1/main.cpp
@@ -7,9 +7,34 @@
 //
 
 #include <iostream>
+#include <math.h>
+
+void matrix(int, int, int);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    std::cout << "N:";
+    int n=0;
+    std::cin >> n;
+    matrix(0, 0,n);
+}
+
+void matrix(int i, int j, int n){
+    if(j<n){
+        if(i<n-1){
+            if(i>=j){
+                std::cout << pow(2, j) << " ";
+                matrix(++i, j, n);
+            }else{
+                std::cout << pow(2, i) << " ";
+                
+                matrix(++i, j, n);
+            }
+        }else{
+            std::cout << pow(2, j);
+            std::cout << std::endl;
+            matrix(0, ++j, n);
+        }
+    }else if(j==n){
+        return;
+    }
 }
