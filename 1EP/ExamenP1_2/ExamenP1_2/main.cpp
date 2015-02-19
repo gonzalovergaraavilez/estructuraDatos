96fb9044c0a4476c67d9e0d4cd805f579bce01a1
Gonzalo Vergara <gonzalo.va@me.com>
2015-02-19 14:28:20 -0600

    EP1_2 actualizado

--- 1EP/ExamenP1_2/ExamenP1_2/main.cpp
+++ 1EP/ExamenP1_2/ExamenP1_2/main.cpp
@@ -11,8 +11,25 @@
 #include "Venta.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    int ventas_totales = 0;
+    Venta *ventas = new Venta[ventas_totales];
+    
+    ventas_totales++;
+    ventas = (Venta *)realloc(ventas, ventas_totales * sizeof(Venta));
+    
     std::cout << "Bienvenido, elija la opción:" << std::endl;
-    std::cout << "1. Agregar venta " << std::endl;
+    std::cout << "1. Agregar persona " << std::endl;
+    int opcion;
+    std::cin >> opcion;
+    opcion--;
+    
+    switch (opcion) {
+        case 0:
+            <#statements#>
+            break;
+            
+        default:
+            break;
+    }
     return 0;
 }
