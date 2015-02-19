b9fac27b1c8ecaa20369c8b2dec7a4a160f14cbd
Gonzalo Vergara <gonzalo.va@me.com>
2015-02-19 13:39:47 -0600

    ejercicio 1 examen

---
+++ pointers/pointers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pointers
+//
+//  Created by Gonzalo Vergara on 2/16/15.
+//  Copyright (c) 2015 Gonzalo Vergara. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
