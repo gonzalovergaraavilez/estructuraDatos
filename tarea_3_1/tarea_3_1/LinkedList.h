d0635c383b4f3fa840fea3a74105eca2354c4dcf
Gonzalo Vergara <gonzalo.va@me.com>
2015-03-03 22:33:20 -0600

    completed exercise 1

---
+++ tarea_3_1/tarea_3_1/LinkedList.h
@@ -0,0 +1,14 @@
+//
+//  LinkedList.h
+//  tarea_3_1
+//
+//  Created by Gonzalo Vergara on 3/3/15.
+//  Copyright (c) 2015 Gonzalo Vergara. All rights reserved.
+//
+
+#ifndef __tarea_3_1__LinkedList__
+#define __tarea_3_1__LinkedList__
+
+#include <stdio.h>
+
+#endif /* defined(__tarea_3_1__LinkedList__) */
---
+++ tarea_3_1/tarea_3_1/Node.h
@@ -0,0 +1,14 @@
+//
+//  Node.h
+//  tarea_3_1
+//
+//  Created by Gonzalo Vergara on 3/3/15.
+//  Copyright (c) 2015 Gonzalo Vergara. All rights reserved.
+//
+
+#ifndef __tarea_3_1__Node__
+#define __tarea_3_1__Node__
+
+#include <stdio.h>
+
+#endif /* defined(__tarea_3_1__Node__) */
