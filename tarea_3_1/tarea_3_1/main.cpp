54d9dd45ad6d08ecaf77a4ef8d21d5e03c593569
Gonzalo Vergara <gonzalo.va@me.com>
2015-03-03 21:58:27 -0600

    added tarea 3

---
+++ tarea_3_1/tarea_3_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tarea_3_1
+//
+//  Created by Gonzalo Vergara on 3/3/15.
+//  Copyright (c) 2015 Gonzalo Vergara. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
