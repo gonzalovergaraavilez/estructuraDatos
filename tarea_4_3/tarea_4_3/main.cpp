ed24794f3fcbfb9e08469e1bc6e5b584eecaaae8
Gonzalo Vergara <gonzalo.va@me.com>
2015-04-02 14:52:43 -0600

    tarea 4_3: goto line ending/beginning, delete words. Insert/delete line, and insert word still up

--- tarea_4_3/tarea_4_3/main.cpp
+++ tarea_4_3/tarea_4_3/main.cpp
@@ -24,7 +24,19 @@ int main(int argc, const char * argv[]) {
 
     
     /*ncurses*/
-    //WINDOW *subwindow;
+    WINDOW *subwindow;
+    WINDOW *textwindow;
+    int offsetx, offsety;
+    
+    initscr();
+    refresh();
+    
+    offsetx = 0;
+    offsety = 10;
+    
+    subwindow = newwin(10, COLS, offsety, offsetx);
+    textwindow = newwin(10, COLS, 0, 0);
+    
     initscr();
     noecho();
     cbreak();
@@ -34,6 +46,9 @@ int main(int argc, const char * argv[]) {
     x = 123;
     clear();
     refresh();
+    int line_length;
+    int word_n = 0;
+    int move_to = 0;
     
     std::ofstream myfile;
         myfile.open ("fil2e.txt");
@@ -61,22 +76,24 @@ int main(int argc, const char * argv[]) {
     
     Node< LinkedList<std::string> * > * tmp = lineas.first();
     Node<std::string> *charnode = tmp->getInfo()->first();
-    clear();
+    wclear(textwindow);
     do{
         while (charnode != nullptr) {
-            printw("%s ",(charnode->getInfo()).c_str()); //addch() for ncurses
+            wprintw(textwindow,"%s ",(charnode->getInfo()).c_str());
             charnode = charnode->getNext();
-            refresh();
+            wrefresh(textwindow);
         }
-        printw("\n");
+        wprintw(textwindow,"\n");
         //std::cout << std::endl;
         tmp = tmp->getNext();
         charnode = tmp->getInfo()->first();
         //std::cout << std::endl;
     }while (tmp != lineas.first());
+    wrefresh(textwindow);
+    move(0, 0);
     /*ncurses loop */
     while((ch = getch()) != 'x'){
-        
+        //printw("key pressed %d",ch);
         getyx(stdscr, y, x);
         switch (ch) {
             case KEY_LEFT:
@@ -91,13 +108,99 @@ int main(int argc, const char * argv[]) {
             case KEY_DOWN:
                 move(y+1, x);
                 break;
+            case 27: //ESC
+                wprintw(textwindow,"exit ket pressed");
+                break;
+            case KEY_SLEFT:
+                getyx(stdscr, y, x);
+                move(y,0);
+                //printw("shifted left arrow");
+                break;
+            case 127:
+                //printw("backspace");
+                //wclear(stdscr);
+                wclear(subwindow);
+                getyx(stdscr, y, x);
+                wprintw(subwindow,"current coords: x: %d, y: %d \n", x, y);
+                tmp = lineas.at(y);
+                if(tmp!=nullptr){
+                    wprintw(subwindow,"\n tmp  not null");
+
+                    charnode = tmp->getInfo()->first();
+                    line_length = 0; //Compensate last word whitespace
+                    word_n = 0;
+                    while (charnode != nullptr) {
+                        if(line_length<=x){
+                            line_length +=(charnode->getInfo()).length();
+                            line_length++; //whitespace
+                            //wprintw(subwindow,"\nline_length: %d", line_length);
+                            word_n++;
+                        }
+                        charnode = charnode->getNext();
+                    }
+                    charnode = tmp->getInfo()->at(word_n-1);
+                    x=line_length-2;
+                    x-=(charnode->getInfo()).length();
+                    if (x<0) x= 0;
+                    wprintw(subwindow,"\nword_n: %d", word_n);
+                    tmp->getInfo()->remove(word_n-1);
+                    
+                    //wprintw(subwindow,"\nword_n content after delete: %s", (tmp->getInfo()->at(word_n-1)->getInfo()).c_str());
+                    
+                    /*charnode = tmp->getInfo()->first();
+                    if(word_n>1){
+                        while (charnode != tmp->getInfo()->at(word_n-2)) {
+                            x+=(charnode->getInfo()).length()+1;
+                            charnode = charnode->getNext();
+                        }
+                        x--; //last whitespace
+                    }*/
+                    move(y, x);
+                    
+                    wrefresh(subwindow);
+                    wrefresh(textwindow);
+                     //reprint
+                    wclear(textwindow);
+                     tmp = lineas.first();
+                     charnode = tmp->getInfo()->first();
+                     do{
+                     while (charnode != nullptr) {
+                     wprintw(textwindow,"%s ",(charnode->getInfo()).c_str());
+                     charnode = charnode->getNext();
+                     refresh();
+                     }
+                     wprintw(textwindow,"\n");
+                     //std::cout << std::endl;
+                     tmp = tmp->getNext();
+                     charnode = tmp->getInfo()->first();
+                     //std::cout << std::endl;
+                     }while (tmp != lineas.first());
+
+                    wrefresh(textwindow);
+                }
+                //wrefresh(subwindow);
+                //refresh();
+                break;
+            case KEY_SRIGHT:
+                getyx(stdscr, y, x);
+                tmp = lineas.at(y);
+                charnode = tmp->getInfo()->first();
+                line_length = -1; //Compensate last word whitespace
+                while (charnode != nullptr) {
+                    line_length +=(charnode->getInfo()).length(); //addch() for ncurses
+                    line_length++; //whitespace
+                    charnode = charnode->getNext();
+                }
+                //printw("%d", line_length);
+                move(y,line_length);
+                break;
         }
         refresh();
         
     }
     /*ncurses*/
     delwin(stdscr);
-    
+    delwin(subwindow);
     endwin();
     
     return 0;
