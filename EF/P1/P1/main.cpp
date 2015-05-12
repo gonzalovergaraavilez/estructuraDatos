6cb01af55443543b6aa43dffb0b754cb92866755
Gonzalo Vergara <gonzalo.va@me.com>
2015-05-12 12:10:23 -0500

    added EF

---
+++ EF/P1/P1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  P1
+//
+//  Created by Gonzalo Vergara on 5/12/15.
+//  Copyright (c) 2015 Gonzalo Vergara. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
